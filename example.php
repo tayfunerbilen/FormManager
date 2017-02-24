0a699b7bf5d0e2a5700310f9e4ed960431025b5d
Tayfun Erbilen <tayfunerbilen@gmail.com>
2017-02-24 17:39:33 +0100

    simple example page

---
+++ example.php
@@ -0,0 +1,22 @@
+<?php
+
+  // include class file
+  require 'formmanager.class.php';
+
+  $form = new FormManager();
+  
+  // create your own form quickly
+  $form->start();
+  $form->input('username', 'Enter username');
+  $form->type('password')->input('password', 'Enter Password');
+  $form->end(false); // means don't show
+  
+  // check before show
+  if($data = $form->control()){
+    print_r($data);
+  } else {
+    echo $form->error();
+  }
+  
+  // you can show where you want :)
+  $form->show();
